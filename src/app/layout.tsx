1a3cc4d34e1ee89fc25d51e84a489fe8534a128e
MMMJB <michaelbeck445@gmail.com>
2026-03-13 14:45:21 -0400

    feat: import fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,17 +2,29 @@ import "@/styles/globals.css";
 
 import Script from "next/script";
 
+import localFont from "next/font/local";
+import { cn } from "@/util/ui";
+
+const generalSans = localFont({
+  src: "../../public/fonts/GS-Variable.woff2",
+  variable: "--font-gs",
+  display: "swap",
+});
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html
+      lang="en"
+      className={cn("font-sans antialiased", generalSans.variable)}
+    >
       <Script id="haptics">{`
         if(!window)throw new Error("Window is not defined");if(!("vibrate"in window.navigator)){const t=document.createElement("input");t.setAttribute("type","checkbox"),t.setAttribute("id","haptics-vibrate"),t.setAttribute("class","hidden"),t.setAttribute("switch","true");const e=document.createElement("label");e.setAttribute("for","haptics-vibrate"),e.setAttribute("class","hidden"),document.body.appendChild(t),document.body.appendChild(e),window.navigator.vibrate=()=>e.click()}
       `}</Script>
-      <body className="antialiased">{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
