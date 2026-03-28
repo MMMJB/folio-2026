19436b2ef4ab1b594292f2631349b631c96f7128
MMMJB <michaelbeck445@gmail.com>
2026-03-28 02:30:37 -0400

    feat: update figcaption styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import WorkPill from "@/components/WorkPill";
 
 export default function Home() {
   return (
-    <div className="mx-auto grid max-w-xl grid-cols-1 gap-y-5 px-6 py-8 text-pretty underline-offset-4 sm:my-20 sm:grid-cols-3 sm:p-0 [&_>_*]:col-span-2">
+    <div className="mx-auto grid max-w-xl grid-cols-1 gap-y-5 px-6 py-8 underline-offset-4 sm:my-20 sm:grid-cols-3 sm:p-0 sm:text-pretty [&_>_*]:col-span-2">
       <div>
         <h1>Michael Beck</h1>
         <span className="text-tx-secondary">Software Engineer</span>
@@ -34,11 +34,11 @@ export default function Home() {
         </span>
       </div>
       <p>
-        I&rsquo;m currently interning at{" "}
+        I&rsquo;m a frontend engineering intern at{" "}
         <WorkPill href="https://pointer.ai" logo="/logos/pointer.png">
           Pointer
         </WorkPill>{" "}
-        as a frontend engineer.
+        and a student at Cornell University.
       </p>
       <p>
         I previously worked at{" "}
@@ -63,34 +63,56 @@ export default function Home() {
         </span>{" "}
         a competitive coding platform.
       </p>
-      <div className="bg-surface-100 relative col-span-full! -mx-3 mt-1 aspect-video overflow-hidden rounded-xl sm:mx-0">
-        <video
-          autoPlay
-          loop
-          muted
-          playsInline
-          preload="metadata"
-          className="bg-skeleton size-full object-contain select-none backface-hidden"
-          width="100%"
-          height="100%"
-          poster="/thumbnails/vars.jpg"
+      <figure className="relative col-span-full! -mx-3 mt-1 aspect-video overflow-hidden rounded-xl">
+        <div
+          style={{
+            "--c": "1.5rem",
+            "--cap-h": "2.5rem",
+            "--cap-w": "9rem",
+            clipPath: `shape(
+              from 0% var(--c),
+              curve to var(--c) 0% with 0% 0%,
+              hline to calc(100% - var(--c)),
+              curve to 100% var(--c) with 100% 0%,
+              vline to calc(100% - var(--c)),
+              curve to calc(100% - var(--c)) 100% with 100% 100%,
+              hline to calc(var(--cap-w) + var(--c)),
+              curve to var(--cap-w) calc(100% - var(--c)) with var(--cap-w) 100%,
+              vline to calc(100% - (var(--cap-h) - var(--c))),
+              curve to calc(var(--cap-w) - var(--c)) calc(100% - var(--cap-h)) with var(--cap-w) calc(100% - var(--cap-h)),
+              hline to var(--c),
+              curve to 0% calc(100% - var(--cap-h) - var(--c)) with 0% calc(100% - var(--cap-h))
+            )`,
+          }}
         >
-          <source src="/videos/vars.webm" type="video/webm" />
-          <source src="/videos/vars.mp4" type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
-        <span className="selection:text-tx-primary absolute bottom-0 left-0 px-4 py-3 text-xs text-white/60">
+          <video
+            autoPlay
+            loop
+            muted
+            playsInline
+            preload="metadata"
+            className="bg-skeleton size-full select-none backface-hidden"
+            width="100%"
+            height="100%"
+            poster="/thumbnails/vars.jpg"
+          >
+            <source src="/videos/vars.webm" type="video/webm" />
+            <source src="/videos/vars.mp4" type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+        </div>
+        <figcaption className="absolute bottom-0 left-0 px-4 py-3 text-xs">
           Shots from{" "}
           <a
             href="https://vars.gg"
             target="_blank"
-            className="underline transition-colors hover:text-white/80"
+            className="underline transition-colors"
           >
             vars.gg
           </a>
           .
-        </span>
-      </div>
+        </figcaption>
+      </figure>
     </div>
   );
 }
